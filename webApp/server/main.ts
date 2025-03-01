0424d87d958ce107ad5f1806822d9575588f1792
czpsychocz1911 <czpsychocz1911@gmail.com>
2025-03-01 10:09:07 +0100

    updated main method for admin acc

--- webApp/server/main.ts
+++ webApp/server/main.ts
@@ -1,5 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 import "../imports/api/methods"
+import { Accounts } from 'meteor/accounts-base';
 
 Meteor.startup(async () => {
+    const admin = await Meteor.users.findOneAsync(({username: "admin"}))
+
+    if(!admin){
+        await Accounts.createUserAsync({username: "admin",password: "admin"})
+    }
 });
