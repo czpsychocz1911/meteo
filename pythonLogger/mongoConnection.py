ddac891efb37c2f86957481747780592db2672aa
czpsychocz1911 <czpsychocz1911@gmail.com>
2025-03-01 18:53:48 +0100

    update

--- pythonLogger/mongoConnection.py
+++ pythonLogger/mongoConnection.py
@@ -1,16 +1,13 @@
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 from schemas import BaseDocument, SoilModel, TempModel , TempUnitsEnum , RelHumidityModel
-import asyncio
 import board
 import busio
 import adafruit_ads1x15.ads1115 as ADS
 from soilMoisture import read_soil_values, calculate_percentage_value_moisture, sensorValue as soilEnum
 from humidityTemp import get_temp, get_relative_humidity
-from time import sleep
-from redisConnection import log_to_redis, SensorRedisKeys
-
-
+from time import sleep, time
+from redisConnection import log_to_redis, SensorRedisKeys, read_from_redis
 
 models = [BaseDocument, SoilModel, TempModel , RelHumidityModel ] 
 
@@ -18,11 +15,15 @@ models = [BaseDocument, SoilModel, TempModel , RelHumidityModel ]
 
 MOISTURE_LOWER_BOUND = 1.258
 MOISTURE_UPPER_BOUND = 3.4
-DEFAULT_WRITING_VALUE = 60
+MONGO_UPDATE_VALUE = 60
+SENSOR_UPDATE_VALUE = 5
 i2c = busio.I2C(board.SCL,board.SDA)
 ads = ADS.ADS1115(i2c)
 
-async def logSoilModel():
+def canUpdateMongo(nextTick: float):
+    return time() >= nextTick
+
+async def logSoilModel(mongoUpdate: bool):
     soilVoltage = read_soil_values(ads,ADS.P1, soilEnum.VOLTAGE)
     soilRawValue = read_soil_values(ads, ADS.P1, soilEnum.VALUE)
     soilPercentage = calculate_percentage_value_moisture(soilVoltage,MOISTURE_UPPER_BOUND,MOISTURE_LOWER_BOUND)
@@ -30,17 +31,21 @@ async def logSoilModel():
     out = SoilModel(ADraw=soilRawValue,ADvolt=soilVoltage,soilHumidity=soilPercentage)
 
     await log_to_redis(SensorRedisKeys.Soil,out)
-    await SoilModel.insert(out)
+    
+    if(mongoUpdate):
+        await SoilModel.insert(out)
 
-async def logTempHumidity():
+async def logTempHumidity(mongoUpdate: bool):
     temp = TempModel(temp=get_temp(i2c),temp_unit=TempUnitsEnum.Celsius)
     relHum = RelHumidityModel(relHum=get_relative_humidity(i2c))
 
     await log_to_redis(SensorRedisKeys.Temp, temp)
     await log_to_redis(SensorRedisKeys.Humidity, relHum)
-    
-    await TempModel.insert(temp)
-    await RelHumidityModel.insert(relHum)
+
+
+    if(mongoUpdate):
+        await TempModel.insert(temp)
+        await RelHumidityModel.insert(relHum)
 
 async def initDB(mongodb_url: str = "mongodb://localhost:28080", db_name: str = "meteor"):
     client = AsyncIOMotorClient(mongodb_url)
@@ -50,10 +55,21 @@ async def initDB(mongodb_url: str = "mongodb://localhost:28080", db_name: str = 
 async def main():
     try:
         await initDB()
+        nextTick = time() + MONGO_UPDATE_VALUE
         while(True):
-            await logSoilModel()
-            await logTempHumidity()
-            sleep(DEFAULT_WRITING_VALUE)
+
+            mongoUpdate = canUpdateMongo(nextTick)
+
+            await logSoilModel(mongoUpdate)
+            await logTempHumidity(mongoUpdate)
+
+            sleep(SENSOR_UPDATE_VALUE)
+
+            if(mongoUpdate):
+                nextTick = time() + MONGO_UPDATE_VALUE
+            
+
+            print(read_from_redis(SensorRedisKeys.Temp))
     except Exception as e:
         print(f"Error happened at: {e}")
     except KeyboardInterrupt:
--- pythonLogger/redisConnection.py
+++ pythonLogger/redisConnection.py
@@ -1,11 +1,12 @@
 import redis
 import json
 from enum import Enum
+import time
 
 class SensorRedisKeys(Enum):
-    Soil = "soil:latest"
-    Temp = "temp:latest"
-    Humidity = "humidity:latest"
+    Soil = "soil"
+    Temp = "temp"
+    Humidity = "humidity"
 
 
 REDIS_EXPIRY = 3600
@@ -15,10 +16,37 @@ REDIS_HOST_PORT = "6379"
 
 redis_client = redis.Redis(REDIS_HOST,REDIS_HOST_PORT)
 
-async def log_to_redis(key,data):
+async def log_to_redis(sensor_type, data):
     try:
-        json_data = json.dumps(data)
-        redis_client.set(key,json_data)
-        redis_client.expire(key, REDIS_EXPIRY)
+
+        #I dont like this code need to make a bit nicer???
+        latest_key = f"{sensor_type.value}"
+        history_key = f"{sensor_type.name.lower()}:history"
+
+        timestamped_data = {
+            "timestamp" : int(time.time()),
+            "data" : data
+        }
+
+        json_data = json.dumps(timestamped_data)
+
+        redis_client.set(latest_key, json_data)
+        redis_client.expire(latest_key, REDIS_EXPIRY)
+
+        redis_client.lpush(history_key, json_data)
+        redis_client.ltrim(history_key, 0 , REDIS_HISTORY_MAX_LEN - 1)
+        redis_client.expire(history_key, REDIS_EXPIRY)
+
     except Exception as e:
         print(f"Redis error: {e}")
+
+async def read_from_redis(sensor_type, limit=REDIS_HISTORY_MAX_LEN):
+    try: 
+        history_key = f"{sensor_type.name.lower()}:history"
+        data = redis_client.lrange(history_key, 0 , limit - 1)
+
+        res = []
+        for item in data:
+            res.append(json.loads(item))
+    except Exception as e:
+        print(f"Failed to retrive data from redis: {e}")
