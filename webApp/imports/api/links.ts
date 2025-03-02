70ac2680c3d3656e96c5b0f5f61e06768f4d972e
czpsychocz1911 <czpsychocz1911@gmail.com>
2025-03-02 14:19:03 +0100

    testing web app mongo

--- webApp/imports/api/links.ts
+++ webApp/imports/api/links.ts
@@ -1,5 +1,11 @@
 import { Mongo } from 'meteor/mongo';
 
+enum TempUnitsEnum {
+  Celsius = "C",
+  Kelvin = "K",
+  Fahrenheit = "F",
+}
+
 export interface Link {
   _id?: string;
   title: string;
@@ -7,4 +13,29 @@ export interface Link {
   createdAt: Date;
 }
 
-export const LinksCollection = new Mongo.Collection<Link>('links');
+export interface BaseModel {
+  _id: string,
+  createdAt: Date
+  updatedAt: Date
+}
+
+export type SoilModel = BaseModel & {
+  soilHumidity: number
+  ADraw: number
+  ADvolt: number
+}
+
+export type TempModel = BaseModel & {
+  temp: number
+  temp_unit: TempUnitsEnum
+}
+
+export type RelHumidityModel = BaseModel & {
+  relHum : number
+}
+
+export const LinksCollection = new Mongo.Collection<Link>('links')
+export const SoilModelCollection = new Mongo.Collection<SoilModel>("soil_moisture")
+export const TempModelCollection = new Mongo.Collection<TempModel>("temperature")
+export const RelHumidityModelCollection = new Mongo.Collection<RelHumidityModel>("relative_humidity")
+
--- webApp/imports/ui/MainPage.tsx
+++ webApp/imports/ui/MainPage.tsx
@@ -1,7 +1,15 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { MainAppBar } from "./MainAppBar"
+import { useTracker } from 'meteor/react-meteor-data';
+import { TempModelCollection } from "../api/links";
 
 export const MainPage: React.FC = () => {
+
+    const tempData = useTracker(() => TempModelCollection.find({}).fetch());
+
+    useEffect(() => {
+        console.log(tempData)
+    },[tempData])
 
     return(
         <div>
