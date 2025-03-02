c67477693ef3d9f8f5c67d10d131e6dc023f062b
czpsychocz1911 <czpsychocz1911@gmail.com>
2025-03-02 11:47:04 +0100

    removed test

--- pythonLogger/main.py
+++ pythonLogger/main.py
@@ -1,8 +1,5 @@
 import asyncio
 from mongoConnection import main
-from redisConnection import test_redis
 
 if __name__ == "__main__":
-    result = asyncio.run(test_redis)
-    print(result)
     asyncio.run(main)
