114b4de56aba668784470f3725441f7aff0f4236
czpsychocz1911 <czpsychocz1911@gmail.com>
2025-03-03 11:14:17 +0100

    testing redis :O

--- webApp/imports/ui/TestComp.tsx
+++ webApp/imports/ui/TestComp.tsx
@@ -4,35 +4,22 @@ import type { SensorDataResponse } from "../api/methods/redisMethods";
 
 export const TestComp : React.FC = () => {
 
-    const [sensorData, setSensorData] = useState<SensorDataResponse>()
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState<string | null>(null)
-
-    const fetchSensorData =  async() => {
-        setLoading(true)
-        setError(null)
-
-        Meteor.callAsync("get.sensor.data", (err : Meteor.Error, res : SensorDataResponse) => {
-            setLoading(false)
-
-            if(err){
-                console.error("Error fetching sensor data", err)
-                setError(err.reason || null)
-            } else {
-                setSensorData(res)
-                console.log("sensor data:", res)
+    const fetchedData = async() => {
+        return Meteor.callAsync("get.sensor.data").then((res) => {
+            console.log(res)
+        }).catch((err) => {
+            if(err instanceof Meteor.Error){
+                console.error(err)
             }
         })
     }
 
     useEffect(() => {
-        const fetchData = async () => {
-            await fetchSensorData()
+        const data = async() => {
+            await fetchedData()
         }
 
-        fetchData()
-
-        const interval = Meteor.setInterval(fetchSensorData,30000)
+        const interval = Meteor.setInterval(fetchedData, 1000)
 
         return () => Meteor.clearInterval(interval)
     })
