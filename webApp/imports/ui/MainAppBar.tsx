713b9cdc15e21a7ca99d7dfbe82c8ef82c0548ad
czpsychocz1911 <czpsychocz1911@gmail.com>
2025-03-03 17:27:29 +0100

    dumbass

--- webApp/imports/ui/MainAppBar.tsx
+++ webApp/imports/ui/MainAppBar.tsx
@@ -1,7 +1,6 @@
 import { AppBar, Button, Menu, MenuItem, Stack } from "@mui/material";
 import React from "react";
 import { UserProfileModal } from "./UserProfileModal";
-import { MainAppBarSensorData } from "./MainAppBarSensorData";
 
 export const MainAppBar: React.FC = () => {
 	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -25,7 +24,6 @@ export const MainAppBar: React.FC = () => {
 					<Button onClick={handleMenu} color="secondary" variant="contained" sx={{marginRight: "0px"}}>
 						Open user settings
 					</Button>
-					<MainAppBarSensorData/>
 					<Menu
 						anchorEl={anchorEl}
 						open={Boolean(anchorEl)}
