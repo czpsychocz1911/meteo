84110b2d122d18033b5e52484870f871c249bc78
czpsychocz1911 <czpsychocz1911@gmail.com>
2025-03-04 14:22:34 +0100

    Finallly working due to my idocy

--- webApp/imports/ui/SensorDataCard.tsx
+++ webApp/imports/ui/SensorDataCard.tsx
@@ -1,7 +1,13 @@
+import { CardContent } from "@mui/material";
+import { Gauge } from "@mui/x-charts";
 import React from "react";
 
-export const SensorDataCard : React.FC = () => {
-    return(
-        <p>Happy happy</p>
-    )
-}
+export const SensorDataCard: React.FC = () => {
+	return (
+		<React.Fragment>
+			<CardContent>
+				<Gauge width={100} height={100} value={50} />
+			</CardContent>
+		</React.Fragment>
+	);
+};
