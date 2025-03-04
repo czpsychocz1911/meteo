16e8be6972f3b62edf7e6c52c8734a29800cacc1
czpsychocz1911 <czpsychocz1911@gmail.com>
2025-03-04 15:25:34 +0100

    lil updatik

--- webApp/imports/ui/SensorDataCard.tsx
+++ webApp/imports/ui/SensorDataCard.tsx
@@ -1,13 +1,26 @@
-import { Card, CardContent } from "@mui/material";
+import { Card, CardContent, Stack } from "@mui/material";
 import { Gauge } from "@mui/x-charts";
 import React from "react";
 
-export const SensorDataCard: React.FC<{ value: number, valMin: number , valMax: number, name: string }> = ({ value, valMin, valMax, name }) => {
+export const SensorDataCard: React.FC<{
+	value: number;
+	valMin: number;
+	valMax: number;
+	name: string;
+}> = ({ value, valMin, valMax, name }) => {
 	return (
 		<Card variant="elevation">
 			<CardContent>
-                <p>{name}</p>
-				<Gauge width={200} height={400} value={value} valueMin={valMin} valueMax={valMax}/>
+				<Stack direction="column" justifyContent="center">
+					<p>{name}</p>
+					<Gauge
+						width={200}
+						height={400}
+						value={value}
+						valueMin={valMin}
+						valueMax={valMax}
+					/>
+				</Stack>
 			</CardContent>
 		</Card>
 	);
