715492b13d89d64821ed916abb490466f8c7f9a2
czpsychocz1911 <czpsychocz1911@gmail.com>
2025-03-04 15:11:12 +0100

    funny message

--- webApp/imports/ui/ContentPage.tsx/MainContentPaper.tsx
+++ webApp/imports/ui/ContentPage.tsx/MainContentPaper.tsx
@@ -1,17 +1,32 @@
-import { Paper } from "@mui/material";
+import { Paper, Stack } from "@mui/material";
 import React from "react";
 import { SensorDataCard } from "../SensorDataCard";
 import { SensorDataProvider, useSensorData } from "../SensorDataContext";
 
-export const MainContentPaper : React.FC = () => {
+export const MainContentPaper: React.FC = () => {
+	const { sensorData } = useSensorData();
 
-    const { sensorData } = useSensorData()
-    
-    return(
-        <div>
-            <Paper elevation={3}>
-                <SensorDataCard value={sensorData?.humidity.data.relHum ?? 0} valMax={100} valMin={0}/>
-            </Paper>
-        </div>
-    )
-}
+	return (
+		<div>
+			<Paper elevation={3}>
+				<Stack direction="row" spacing={4}>
+					<SensorDataCard
+						value={sensorData?.humidity.data.relHum ?? 0}
+						valMax={100}
+						valMin={0}
+					/>
+					<SensorDataCard
+						value={sensorData?.temp.data.temp ?? 0}
+						valMax={40}
+						valMin={20}
+					/>
+					<SensorDataCard
+						value={sensorData?.soil.data.soilHumidity ?? 0}
+						valMin={0}
+						valMax={0}
+					/>
+				</Stack>
+			</Paper>
+		</div>
+	);
+};
