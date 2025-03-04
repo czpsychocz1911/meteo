cc73f4cff52278ded78b8bb32001eae66b96d706
czpsychocz1911 <czpsychocz1911@gmail.com>
2025-03-04 14:51:03 +0100

    added comp

--- webApp/imports/ui/ContentPage.tsx/MainContentPaper.tsx
+++ webApp/imports/ui/ContentPage.tsx/MainContentPaper.tsx
@@ -1,11 +1,12 @@
 import { Paper } from "@mui/material";
 import React from "react";
+import { SensorDataCard } from "../SensorDataCard";
 
 export const MainContentPaper : React.FC = () => {
     return(
         <div>
             <Paper elevation={3}>
-                
+                <SensorDataCard/>
             </Paper>
         </div>
     )
