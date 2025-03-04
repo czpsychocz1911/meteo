aaeb68900b3b21e317f7ea7368ef2615815368bf
czpsychocz1911 <czpsychocz1911@gmail.com>
2025-03-04 12:58:43 +0100

    updated sensor comp

--- webApp/imports/ui/SensorCompLiveFeed.tsx
+++ webApp/imports/ui/SensorCompLiveFeed.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-import { useSensorData } from "./SensorDataContext";
-import { SensorDataComp } from "./SensorDataComp";
+import { useSensorData } from "./SensorContext";
 import { SensorComp } from "./SensorComp";
 
-export const SensorCompLiveFeed : React.FC = () => {
-
-    const { sensorData } = useSensorData()
-
-    return(
-        <SensorDataComp>
+export const SensorCompLiveFeed: React.FC = () => {
+    const { sensorData } = useSensorData();
+    
+    return (
+        <>
             <SensorComp unit={sensorData?.temp.data.temp_unit ?? "C"} value={sensorData?.temp.data.temp ?? ""} sensorType="Temperature"/>
             <SensorComp unit={"%"} value={sensorData?.soil.data.soilHumidity ?? ""} sensorType="Soil humidity"/>
             <SensorComp unit={"%"} value={sensorData?.humidity.data.relHum ?? ""} sensorType="Humidity"/>
-        </SensorDataComp>
-    )
+        </>
+    );
 }
