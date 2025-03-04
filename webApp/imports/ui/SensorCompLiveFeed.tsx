842596b8bb3e23ca587bdf8e804024f79e5adcc6
czpsychocz1911 <czpsychocz1911@gmail.com>
2025-03-04 13:01:38 +0100

    lil updatik

--- webApp/imports/ui/SensorCompLiveFeed.tsx
+++ webApp/imports/ui/SensorCompLiveFeed.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { SensorComp } from "./SensorComp";
-import { useSensorData } from "./SensorDataContext";
+import { SensorDataProvider, useSensorData } from "./SensorDataContext";
 
 export const SensorCompLiveFeed: React.FC = () => {
-    const { sensorData } = useSensorData();
-    
-    return (
-        <>
-            <SensorComp unit={sensorData?.temp.data.temp_unit ?? "C"} value={sensorData?.temp.data.temp ?? ""} sensorType="Temperature"/>
-            <SensorComp unit={"%"} value={sensorData?.soil.data.soilHumidity ?? ""} sensorType="Soil humidity"/>
-            <SensorComp unit={"%"} value={sensorData?.humidity.data.relHum ?? ""} sensorType="Humidity"/>
-        </>
-    );
-}
+	const { sensorData } = useSensorData();
+
+	return (
+		<SensorDataProvider>
+			<SensorComp
+				unit={sensorData?.temp.data.temp_unit ?? "C"}
+				value={sensorData?.temp.data.temp ?? ""}
+				sensorType="Temperature"
+			/>
+			<SensorComp
+				unit={"%"}
+				value={sensorData?.soil.data.soilHumidity ?? ""}
+				sensorType="Soil humidity"
+			/>
+			<SensorComp
+				unit={"%"}
+				value={sensorData?.humidity.data.relHum ?? ""}
+				sensorType="Humidity"
+			/>
+		</SensorDataProvider>
+	);
+};
